20338109d34c113c619ad90ab9f7655c19cb74e7
zhichuande <zhichuande@users.noreply.github.com>
2017-09-09 10:28:51 +0800

    Update CryptoNoteConfig.h
    加入种子节点

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
+  "18.221.76.10",
   //"your_seed_ip2.com:8080",
 };
 
